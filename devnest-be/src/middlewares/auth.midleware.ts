47d3a9a432006b7adb744e9931bb5a6ef9cbcbd3
Le Dinh Dung <dev.dungld@gmail.com>
2026-03-05 12:16:16 +0700

    feature/api-get-user

--- devnest-be/src/middlewares/auth.midleware.ts
+++ devnest-be/src/middlewares/auth.midleware.ts
@@ -4,6 +4,11 @@ import { Request, NextFunction, Response } from "express";
 export interface AuthRequest extends Request {
 	user?: any;
 }
+interface JwtPayload {
+	userId: string;
+	role: string;
+}
+
 export const protect = (
 	req: AuthRequest,
 	res: Response,
@@ -27,4 +32,31 @@ export const authorize = (...roles: string[]) => {
 		}
 		next();
 	};
+};
+
+export const verifyAccessToken = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const authHeader = req.headers.authorization;
+
+		if (!authHeader || !authHeader.startsWith("Bearer ")) {
+			return res.status(401).json({ message: "Unauthorized" });
+		}
+
+		const token = authHeader.split(" ")[1];
+
+		const decoded = jwt.verify(
+			token,
+			process.env.JWT_ACCESS_SECRET!,
+		) as JwtPayload;
+
+		(req as any).user = decoded;
+
+		next();
+	} catch (error) {
+		return res.status(401).json({ message: "Token expired or invalid" });
+	}
 };
